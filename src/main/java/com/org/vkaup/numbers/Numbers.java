9da961cc1c3fc347fe32c730b83bec2e0fe355fc
Vidhir Kaup <vkaup@Vidhirs-MacBook-Pro.local>
2020-11-29 15:51:03 -0500

    returns sum

--- src/main/java/com/org/vkaup/numbers/Numbers.java
+++ src/main/java/com/org/vkaup/numbers/Numbers.java
@@ -2,6 +2,7 @@ package com.org.vkaup.numbers;
 
 public class Numbers {
 
+    // Returns the sum of 2 numbers
     public int sum(int a, int b) throws ArithmeticException {
         return a + b;
     }
